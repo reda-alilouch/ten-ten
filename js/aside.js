1a6483ba3a3d956436b60947d6b0eccfec9defd9
reda-alilouch <alilouchreda328black@gmail.com>
2024-06-27 21:08:45 +0100

    feat/3 sidebar

--- js/aside.js
+++ js/aside.js
@@ -1,38 +1,37 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-   
-    const showNavbar = (toggleId, sideId, bodyId, asideId) =>{
+document.addEventListener("DOMContentLoaded", function (event) {
+  const showSidebar = (toggleId, sideId, bodyId, asideId) => {
     const toggle = document.getElementById(toggleId),
-    side = document.getElementById(sideId),
-    bodypd = document.getElementById(bodyId),
-    asidepd = document.getElementById(asideId)
-    
+      sidebar = document.getElementById(sideId),
+      bodypd = document.getElementById(bodyId),
+      asidepd = document.getElementById(asideId);
+
     // Validate that all variables exist
-    if(toggle && side && bodypd && asidepd){
-    toggle.addEventListener('click', ()=>{
-    // show navbar
-    side.classList.toggle('show')
-    // change icon
-    toggle.classList.toggle('bx-x')
-    // add padding to body
-    bodypd.classList.toggle('body-pd')
-    // add padding to header
-    headerpd.classList.toggle('body-pd')
-    })
+    if (toggle && sidebar && bodypd && asidepd) {
+      toggle.addEventListener("click", () => {
+        // show navbar
+        side.classList.toggle("show");
+        // change icon
+        toggle.classList.toggle("fa");
+        // add padding to body
+        bodypd.classList.toggle("body-pd");
+        // add padding to header
+        asidepd.classList.toggle("body-pd");
+      });
     }
+  };
+
+  showSidebar("aside-toggle", "sidebar", "body-pd", "aside");
+
+  /*===== LINK ACTIVE =====*/
+  const linkColor = document.querySelectorAll(".side_link");
+
+  function colorLink() {
+    if (linkColor) {
+      linkColor.forEach((l) => l.classList.remove("active"));
+      this.classList.add("active");
     }
-    
-    showNavbar('aside-toggle','side-bar','body-pd','aside')
-    
-    /*===== LINK ACTIVE =====*/
-    const linkColor = document.querySelectorAll('.side_link')
-    
-    function colorLink(){
-    if(linkColor){
-    linkColor.forEach(l=> l.classList.remove('active'))
-    this.classList.add('active')
-    }
-    }
-    linkColor.forEach(l=> l.addEventListener('click', colorLink))
-    
-     // Your code to run since DOM is loaded and ready
-    });
+  }
+  linkColor.forEach((l) => l.addEventListener("click", colorLink));
+
+  // Your code to run since DOM is loaded and ready
+});
